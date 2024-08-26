a953fa5eb82be884cd624c9b6263c0983252d4f8
solkeen <nakao95911@gmail.com>
2024-08-27 05:14:36 +0800

    add: get route in ray-snip-bot commit by solkeen

--- routes/SnipingRoute/RaydiumRoute.ts
+++ routes/SnipingRoute/RaydiumRoute.ts
@@ -3,6 +3,10 @@ import { Router } from "express";
 // Create a new instance of the Express Router
 const RaydiumSnipingRoute = Router();
 
+RaydiumSnipingRoute.get("/", async (req, res) => {
+  res.send({msg : "GET Raydium Sniping Bot"})
+});
+
 RaydiumSnipingRoute.post("/startbot", async (req, res) => {
   console.log(req.body)
   res.send({msg : "sucess"})
