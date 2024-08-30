1d9bed2b4f19c2895d3e2a914a303807ad131051
solkeen <nakao95911@gmail.com>
2024-08-30 18:02:56 +0900

    update: backend fix commit by solkeen

--- config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BACKEND_URL = exports.FRONTEND_URL = exports.BACKEND_PORT = void 0;
-var config_1 = require("./config");
-Object.defineProperty(exports, "BACKEND_PORT", { enumerable: true, get: function () { return config_1.BACKEND_PORT; } });
-Object.defineProperty(exports, "FRONTEND_URL", { enumerable: true, get: function () { return config_1.FRONTEND_URL; } });
-Object.defineProperty(exports, "BACKEND_URL", { enumerable: true, get: function () { return config_1.BACKEND_URL; } });
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RaydiumSnipingRoute = void 0;
-const SnipingRoute_1 = require("./SnipingRoute");
-Object.defineProperty(exports, "RaydiumSnipingRoute", { enumerable: true, get: function () { return SnipingRoute_1.RaydiumSnipingRoute; } });
